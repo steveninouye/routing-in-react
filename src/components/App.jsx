763b3710512ad8950c966dfb7401fc369f8b3e81
Steven Inouye <steveninouye@msn.com>
2018-08-19 20:37:06 -1000

    installed react-router-dom

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>hello from app</h1>;
+    }
+}
+
+export default App;
